d411ba2201f110efc6c1f6fa7648304391197572
hugoj-goncalves <hugo159@gmail.com>
2020-02-07 10:53:34 -0300

    Reverting changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import autoExternal from 'rollup-plugin-auto-external';
-import uglify from 'rollup-plugin-uglify-es';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   format: 'umd',
