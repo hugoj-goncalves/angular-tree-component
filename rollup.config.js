400cc26a4039ab04c54d7bee6e10980ae6dac7de
hugoj-goncalves <hugo159@gmail.com>
2020-02-07 09:54:44 -0300

    WIP

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import autoExternal from 'rollup-plugin-auto-external';
-import uglify from 'rollup-plugin-uglify';
+import uglify from 'rollup-plugin-uglify-es';
 
 export default {
   format: 'umd',
